7c035b14bc5274b6d5711a5760906955f0c13780
levanminh04 <levanminh031204@gmail.com>
2025-02-14 21:57:24 +0700

    first commit

---
+++ src/main/java/levanminh/Whatsapp_Clone_BE/WhatsappCloneBeApplication.java
@@ -0,0 +1,13 @@
+package levanminh.Whatsapp_Clone_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatsappCloneBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatsappCloneBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/levanminh/Whatsapp_Clone_BE/WhatsappCloneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package levanminh.Whatsapp_Clone_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatsappCloneBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
